8b051640680a0200fe55ddc7e05895a1ab6f6f39
Vandebrewski <Bastiaan@MacBook-Pro-van-Bastiaan-2.local>
2016-07-22 19:57:56 +0200

    upd

---
+++ www/js/controllers.js
@@ -0,0 +1,393 @@
+angular.module('your_app_name.controllers', [])
+
+.controller('AuthCtrl', function($scope, $ionicConfig) {
+
+})
+
+// APP
+.controller('AppCtrl', function($scope, $ionicConfig) {
+
+})
+
+.controller('ProfileCtrl', function($scope) {
+	$scope.image = 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg';
+})
+
+
+
+.controller('RateApp', function($scope) {
+	$scope.rateApp = function(){
+		if(ionic.Platform.isIOS()){
+			//you need to set your own ios app id
+			AppRate.preferences.storeAppURL.ios = '1234555553>';
+			AppRate.promptForRating(true);
+		}else if(ionic.Platform.isAndroid()){
+			//you need to set your own android app id
+			AppRate.preferences.storeAppURL.android = 'market://details?id=ionFB';
+			AppRate.promptForRating(true);
+		}
+	};
+})
+
+
+
+.controller('AdsCtrl', function($scope, $ionicActionSheet, AdMob, iAd) {
+
+	$scope.manageAdMob = function() {
+
+		// Show the action sheet
+		var hideSheet = $ionicActionSheet.show({
+			//Here you can add some more buttons
+			buttons: [
+				{ text: 'Show Banner' },
+				{ text: 'Show Interstitial' }
+			],
+			destructiveText: 'Remove Ads',
+			titleText: 'Choose the ad to show',
+			cancelText: 'Cancel',
+			cancel: function() {
+				// add cancel code..
+			},
+			destructiveButtonClicked: function() {
+				console.log("removing ads");
+				AdMob.removeAds();
+				return true;
+			},
+			buttonClicked: function(index, button) {
+				if(button.text == 'Show Banner')
+				{
+					console.log("show banner");
+					AdMob.showBanner();
+				}
+
+				if(button.text == 'Show Interstitial')
+				{
+					console.log("show interstitial");
+					AdMob.showInterstitial();
+				}
+
+				return true;
+			}
+		});
+	};
+
+	$scope.manageiAd = function() {
+
+		// Show the action sheet
+		var hideSheet = $ionicActionSheet.show({
+			//Here you can add some more buttons
+			buttons: [
+			{ text: 'Show iAd Banner' },
+			{ text: 'Show iAd Interstitial' }
+			],
+			destructiveText: 'Remove Ads',
+			titleText: 'Choose the ad to show - Interstitial only works in iPad',
+			cancelText: 'Cancel',
+			cancel: function() {
+				// add cancel code..
+			},
+			destructiveButtonClicked: function() {
+				console.log("removing ads");
+				iAd.removeAds();
+				return true;
+			},
+			buttonClicked: function(index, button) {
+				if(button.text == 'Show iAd Banner')
+				{
+					console.log("show iAd banner");
+					iAd.showBanner();
+				}
+				if(button.text == 'Show iAd Interstitial')
+				{
+					console.log("show iAd interstitial");
+					iAd.showInterstitial();
+				}
+				return true;
+			}
+		});
+	};
+})
+
+// FEED
+//brings all feed categories
+.controller('FeedsCategoriesCtrl', function($scope, $http) {
+	$scope.feeds_categories = [];
+
+	$http.get('feeds-categories.json').success(function(response) {
+		$scope.feeds_categories = response;
+	});
+})
+
+//bring specific category providers
+.controller('CategoryFeedsCtrl', function($scope, $http, $stateParams) {
+	$scope.category_sources = [];
+
+	$scope.categoryId = $stateParams.categoryId;
+
+	$http.get('feeds-categories.json').success(function(response) {
+		var category = _.find(response, {id: $scope.categoryId});
+		$scope.categoryTitle = category.title;
+		$scope.category_sources = category.feed_sources;
+	});
+})
+
+//this method brings posts for a source provider
+.controller('FeedEntriesCtrl', function($scope, $stateParams, $http, FeedList, $q, $ionicLoading, BookMarkService) {
+	$scope.feed = [];
+
+	var categoryId = $stateParams.categoryId,
+			sourceId = $stateParams.sourceId;
+
+	$scope.doRefresh = function() {
+
+		$http.get('feeds-categories.json').success(function(response) {
+
+			$ionicLoading.show({
+				template: 'Loading entries...'
+			});
+
+			var category = _.find(response, {id: categoryId }),
+					source = _.find(category.feed_sources, {id: sourceId });
+
+			$scope.sourceTitle = source.title;
+
+			FeedList.get(source.url)
+			.then(function (result) {
+				$scope.feed = result.feed;
+				$ionicLoading.hide();
+				$scope.$broadcast('scroll.refreshComplete');
+			}, function (reason) {
+				$ionicLoading.hide();
+				$scope.$broadcast('scroll.refreshComplete');
+			});
+		});
+	};
+
+	$scope.doRefresh();
+
+	$scope.bookmarkPost = function(post){
+		$ionicLoading.show({ template: 'Post Saved!', noBackdrop: true, duration: 1000 });
+		BookMarkService.bookmarkFeedPost(post);
+	};
+})
+
+// SETTINGS
+.controller('SettingsCtrl', function($scope, $ionicActionSheet, $state) {
+	$scope.airplaneMode = true;
+	$scope.wifi = false;
+	$scope.bluetooth = true;
+	$scope.personalHotspot = true;
+
+	$scope.checkOpt1 = true;
+	$scope.checkOpt2 = true;
+	$scope.checkOpt3 = false;
+
+	$scope.radioChoice = 'B';
+
+	// Triggered on a the logOut button click
+	$scope.showLogOutMenu = function() {
+
+		// Show the action sheet
+		var hideSheet = $ionicActionSheet.show({
+			//Here you can add some more buttons
+			// buttons: [
+			// { text: '<b>Share</b> This' },
+			// { text: 'Move' }
+			// ],
+			destructiveText: 'Logout',
+			titleText: 'Are you sure you want to logout? This app is awsome so I recommend you to stay.',
+			cancelText: 'Cancel',
+			cancel: function() {
+				// add cancel code..
+			},
+			buttonClicked: function(index) {
+				//Called when one of the non-destructive buttons is clicked,
+				//with the index of the button that was clicked and the button object.
+				//Return true to close the action sheet, or false to keep it opened.
+				return true;
+			},
+			destructiveButtonClicked: function(){
+				//Called when the destructive button is clicked.
+				//Return true to close the action sheet, or false to keep it opened.
+				$state.go('auth.walkthrough');
+			}
+		});
+
+	};
+})
+
+
+
+// BOOKMARKS
+.controller('BookMarksCtrl', function($scope, $rootScope, BookMarkService, $state) {
+
+	$scope.bookmarks = BookMarkService.getBookmarks();
+
+	// When a new post is bookmarked, we should update bookmarks list
+	$rootScope.$on("new-bookmark", function(event){
+		$scope.bookmarks = BookMarkService.getBookmarks();
+	});
+
+	$scope.goToFeedPost = function(link){
+		window.open(link, '_blank', 'location=yes');
+	};
+	$scope.goToWordpressPost = function(postId){
+		$state.go('app.post', {postId: postId});
+	};
+})
+
+// WORDPRESS
+.controller('WordpressCtrl', function($scope, $http, $ionicLoading, PostService, BookMarkService) {
+	$scope.posts = [];
+	$scope.page = 1;
+	$scope.totalPages = 1;
+
+	$scope.doRefresh = function() {
+		$ionicLoading.show({
+			template: 'Loading posts...'
+		});
+
+		//Always bring me the latest posts => page=1
+		PostService.getRecentPosts(1)
+		.then(function(data){
+			$scope.totalPages = data.pages;
+			$scope.posts = PostService.shortenPosts(data.posts);
+
+			$ionicLoading.hide();
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	};
+
+	$scope.loadMoreData = function(){
+		$scope.page += 1;
+
+		PostService.getRecentPosts($scope.page)
+		.then(function(data){
+			//We will update this value in every request because new posts can be created
+			$scope.totalPages = data.pages;
+			var new_posts = PostService.shortenPosts(data.posts);
+			$scope.posts = $scope.posts.concat(new_posts);
+
+			$scope.$broadcast('scroll.infiniteScrollComplete');
+		});
+	};
+
+	$scope.moreDataCanBeLoaded = function(){
+		return $scope.totalPages > $scope.page;
+	};
+
+	$scope.bookmarkPost = function(post){
+		$ionicLoading.show({ template: 'Post Saved!', noBackdrop: true, duration: 1000 });
+		BookMarkService.bookmarkWordpressPost(post);
+	};
+
+	$scope.doRefresh();
+})
+
+// WORDPRESS2
+.controller('WordpressCtrl2', function($scope, $http, $ionicLoading, PostService2, BookMarkService) {
+	$scope.posts = [];
+	$scope.page = 1;
+	$scope.totalPages = 1;
+
+	$scope.doRefresh = function() {
+		$ionicLoading.show({
+			template: 'Loading posts...'
+		});
+
+		//Always bring me the latest posts => page=1
+		PostService2.getRecentPosts(1)
+		.then(function(data){
+			$scope.totalPages = data.pages;
+			$scope.posts = PostService2.shortenPosts(data.posts);
+
+			$ionicLoading.hide();
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	};
+
+	$scope.loadMoreData = function(){
+		$scope.page += 1;
+
+		PostService2.getRecentPosts($scope.page)
+		.then(function(data){
+			//We will update this value in every request because new posts can be created
+			$scope.totalPages = data.pages;
+			var new_posts = PostService2.shortenPosts(data.posts);
+			$scope.posts = $scope.posts.concat(new_posts);
+
+			$scope.$broadcast('scroll.infiniteScrollComplete');
+		});
+	};
+
+	$scope.moreDataCanBeLoaded = function(){
+		return $scope.totalPages > $scope.page;
+	};
+
+	$scope.bookmarkPost = function(post){
+		$ionicLoading.show({ template: 'Post Saved!', noBackdrop: true, duration: 1000 });
+		BookMarkService.bookmarkWordpressPost(post);
+	};
+
+	$scope.doRefresh();
+})
+
+// WORDPRESS POST
+.controller('WordpressPostCtrl', function($scope, post_data, $ionicLoading) {
+
+	$scope.post = post_data.post;
+	$ionicLoading.hide();
+
+	$scope.sharePost = function(link){
+		window.plugins.socialsharing.share('Check this post here: ', null, null, link);
+	};
+})
+
+
+// WORDPRESS POST2
+//.controller('WordpressPostCtrl2', function($scope, post_data, $ionicLoading) {
+
+//	$scope.post = post_data.post;
+//	$ionicLoading.hide();
+
+//	$scope.sharePost = function(link){
+//		window.plugins.socialsharing.share('Check this post here: ', null, null, link);
+//	};
+//})
+
+.controller('ImagePickerCtrl', function($scope, $rootScope, $ionicPlatform, $cordovaImagePicker) {
+
+	$scope.images = [];
+
+	$scope.selImages = function() {
+
+		//We use image picker plugin: http://ngcordova.com/docs/plugins/imagePicker/
+    //implemented for iOS and Android 4.0 and above.
+
+    $ionicPlatform.ready(function() {
+      $cordovaImagePicker.getPictures()
+       .then(function (results) {
+          for (var i = 0; i < results.length; i++) {
+            console.log('Image URI: ' + results[i]);
+            $scope.status_post.images.push(results[i]);
+          }
+        }, function(error) {
+          // error getting photos
+        });
+    });
+	};
+
+	$scope.removeImage = function(image) {
+		$scope.images = _.without($scope.images, image);
+	};
+
+	$scope.shareImage = function(image) {
+		window.plugins.socialsharing.share(null, null, image);
+	};
+
+	$scope.shareAll = function() {
+		window.plugins.socialsharing.share(null, null, $scope.images);
+	};
+})
+
+;

9e7a33fb5fc3f8e5261b8981d15edb7de981990e
Vandebrewski <Bastiaan@MacBook-Pro-van-Bastiaan.local>
2016-10-25 14:27:48 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ angular.module('your_app_name', [
   // This fixes transitions for transparent background views
   $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
    
-    AdMob.showBanner(); // begint bovenaan en na een paar clicks onderaan
+   // AdMob.showBanner(); // begint bovenaan en na een paar clicks onderaan. laat lege ads zien
     
     if(toState.name.indexOf('auth.walkthrough') > -1)
     {
